106813e37718c1cac7d64046bcb3c1fd835f741d
Debora Gama <gama.debora1@gmail.com>
2022-07-30 17:44:25 -0300

    docs: installing uuid lib

--- exercicio_estacionamento.py
+++ exercicio_estacionamento.py
@@ -5,6 +5,23 @@
 #  - 25 vagas para motos e 25 vagas para carros
 #  - carros e motos identificados por suas placas
 #  - vagas identificadas por um número (id único)
-#  - carros só estacionam em vagas para carros, motos estacionam preferncialmente em vagas para motos, mas podem estacionar em vagas de carro caso não hajam mais vagas de moto disponíveis - 2 motos por vaga
-#  - quando o dono vier buscar o carro, devemos poder saber automaticamente qual carro estpa em qual vaga (placa X- vaga - id X)
+#  - carros só estacionam em vagas para carros, motos estacionam prefrencialmente em vagas para motos, mas podem estacionar em vagas de carro caso não hajam mais vagas de moto disponíveis
+#  - quando o dono vier buscar o carro, devemos poder saber automaticamente qual carro está em qual vaga (placa X- vaga - id X)
 #  - deve serpossível saber quantas vagas ainda estão disponíveis para estacionar- motos e carros
+
+# import uuid
+
+class Estacionamento:
+    def __init__(self):
+        self.vagas_carro = 25
+        self.vagas_moto = 25
+        self.vagas_livres_carro = 25
+        self.vagas_livres_moto = 25
+
+
+class Vaga:
+    def __init__(self):
+        self.livre = false
+
+    
+        
