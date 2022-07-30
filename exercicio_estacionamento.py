eead795bfd01088de10601d40dd249b1ce36afeb
Debora Gama <gama.debora1@gmail.com>
2022-07-30 17:26:21 -0300

    feat: describing business rules

---
+++ exercicio_estacionamento.py
@@ -0,0 +1,10 @@
+# REGRAS DE NEGÓCIO
+
+# ESTACIONAMENTO:
+#  - pátio com 50 vagas
+#  - 25 vagas para motos e 25 vagas para carros
+#  - carros e motos identificados por suas placas
+#  - vagas identificadas por um número (id único)
+#  - carros só estacionam em vagas para carros, motos estacionam preferncialmente em vagas para motos, mas podem estacionar em vagas de carro caso não hajam mais vagas de moto disponíveis - 2 motos por vaga
+#  - quando o dono vier buscar o carro, devemos poder saber automaticamente qual carro estpa em qual vaga (placa X- vaga - id X)
+#  - deve serpossível saber quantas vagas ainda estão disponíveis para estacionar- motos e carros
